c1171f692bf868418983e154dd35f9e65f973d8b
OGTutor <i.send.you.a.letter@gmail.com>
2023-05-22 20:16:01 +0300

    Implemented AuthSocialButton components & finalized login/register fields

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,9 +2,12 @@
 
 import { FC, useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 import Button from '@/components/Button';
 import Input from '@/components/inputs/Input';
+
+import AuthSocialButton from './AuthSocialButton';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -60,6 +63,7 @@ const AuthForm: FC = () => {
 							label="Name"
 							register={register}
 							errors={errors}
+							disabled={isLoading}
 						/>
 					)}
 					<Input
@@ -68,6 +72,7 @@ const AuthForm: FC = () => {
 						type="email"
 						register={register}
 						errors={errors}
+						disabled={isLoading}
 					/>
 					<Input
 						id="password"
@@ -75,6 +80,7 @@ const AuthForm: FC = () => {
 						type="password"
 						register={register}
 						errors={errors}
+						disabled={isLoading}
 					/>
 					<div>
 						<Button disabled={isLoading} fullWidth type="submit">
@@ -82,6 +88,41 @@ const AuthForm: FC = () => {
 						</Button>
 					</div>
 				</form>
+				<div className="mt-6">
+					<div className="relative">
+						<div className="absolute inset-0 flex items-center">
+							<div className="w-full border-t border-gray-300"></div>
+						</div>
+						<div className="relative flex justify-center text-sm">
+							<span className="bg-white px-2 text-gray-500">
+								Or continue with
+							</span>
+						</div>
+					</div>
+					<div className="mt-6 flex gap-2">
+						<AuthSocialButton
+							icon={BsGithub}
+							onClick={() => socialAction('github')}
+						/>
+						<AuthSocialButton
+							icon={BsGoogle}
+							onClick={() => socialAction('google')}
+						/>
+					</div>
+				</div>
+				<div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+					<div>
+						{variant === 'LOGIN'
+							? 'New to Messenger?'
+							: 'Already have an account?'}
+					</div>
+					<div
+						onClick={toggleVariant}
+						className="underline cursor-pointer"
+					>
+						{variant === 'LOGIN' ? 'Create an account' : 'Login'}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
