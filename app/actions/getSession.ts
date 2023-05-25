6173a6e6201f431c0ec42c5797bf0c7658e216b4
OGTutor <i.send.you.a.letter@gmail.com>
2023-05-25 21:29:25 +0300

    Added getCurrentUser & getSession actions

---
+++ app/actions/getSession.ts
@@ -0,0 +1,6 @@
+import { authOptions } from 'api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+
+export default async function getSession() {
+	return await getServerSession(authOptions);
+}
