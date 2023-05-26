9eec393d8815ac92f5f9b26dfb501ec43349d1ea
OGTutor <i.send.you.a.letter@gmail.com>
2023-05-26 20:47:41 +0300

    Implemented & added users screen

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,22 @@
 import Sidebar from '@/components/sidebar/Sidebar';
 
+import UserList from './components/UserList';
+import getUsers from '@/actions/getUsers';
+
 export default async function UsersLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const users = await getUsers();
+
 	return (
 		// @ts-expect-error Server Component
 		<Sidebar>
-			<div className="h-full">{children}</div>
+			<div className="h-full">
+				<UserList items={users} />
+				{children}
+			</div>
 		</Sidebar>
 	);
 }
