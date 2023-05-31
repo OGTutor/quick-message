c8ffbc004a752218b20ddaa55a0aa3f32a4b974f
OGTutor <i.send.you.a.letter@gmail.com>
2023-05-31 22:23:41 +0300

    Implemented getConversationById & getMessages actions

---
+++ app/actions/getConversationById.ts
@@ -0,0 +1,23 @@
+import getCurrentUser from './getCurrentUser';
+import prisma from '@/libs/prismadb';
+
+const getConversationById = async (conversationId: string) => {
+	try {
+		const currentUser = await getCurrentUser();
+
+		if (!currentUser?.email) return null;
+
+		const conversation = await prisma.conversation.findUnique({
+			where: {
+				id: conversationId,
+			},
+			include: { users: true },
+		});
+
+		return conversation;
+	} catch (error: any) {
+		return null;
+	}
+};
+
+export default getConversationById;
