c9d208216def2c3cadf0b68cf9bac6a911e46345
OGTutor <i.send.you.a.letter@gmail.com>
2023-06-12 21:43:23 +0300

    Added a LoadingModal component

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { FC, useCallback, useState } from 'react';
 
 import Avatar from '@/components/Avatar';
+import LoadingModal from '@/components/LoadingModal';
 
 interface UserBoxProps {
 	data: User;
@@ -29,22 +30,25 @@ const UserBox: FC<UserBoxProps> = ({ data }) => {
 	}, [data, router]);
 
 	return (
-		<div
-			onClick={handleClick}
-			className="w-full relative flex items-center space-x-3 bg-white
+		<>
+			{isLoading && <LoadingModal />}
+			<div
+				onClick={handleClick}
+				className="w-full relative flex items-center space-x-3 bg-white
 			p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-		>
-			<Avatar user={data} />
-			<div className="min-w-0 flex-1">
-				<div className="focus:outline-none">
-					<div className="flex justify-between items-center mb-1">
-						<p className="text-sm font-medium text-gray-900">
-							{data.name}
-						</p>
+			>
+				<Avatar user={data} />
+				<div className="min-w-0 flex-1">
+					<div className="focus:outline-none">
+						<div className="flex justify-between items-center mb-1">
+							<p className="text-sm font-medium text-gray-900">
+								{data.name}
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
